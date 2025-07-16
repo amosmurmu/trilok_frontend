5728d413e444909f50cc482f5c1cb16eee671f10
amosmurmu <amosmurmu002@gmail.com>
2025-07-16 20:07:07 +0530

    impl: basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import HomePage from "./components/HomePage"
+import { AboutPage } from "./components/AboutPage"
+import { ContactUs } from "./components/ContactUs"
+import { ProductsPage } from "./components/ProductsPage"
+import { ProjectDetailPage } from "./components/ProjectDetailPage"
+import { ProjectsPage } from "./components/ProjectsPage"
 
 function App() {
 
@@ -8,6 +13,11 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactUs />} />
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="/projects/:id" element={<ProjectDetailPage />} />
         </Routes>
       </Router>
     </>
--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,6 +1,9 @@
+import { NavBar } from "./Navbar";
+
 export default function HomePage() {
   return (
     <>
+      <NavBar />
       <h1 className="text-3xl font-bold underline">This is initial title for trilok</h1>
     </>
   )
