e6b4a07180a36c680b98a91b18d371af093720a7
amosmurmu <amosmurmu002@gmail.com>
2025-07-16 19:14:29 +0530

    impl: shadcn integration

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import { NavigationMenu, NavigationMenuItem } from "./ui/navigation-menu";
+
+export function NavBar() {
+  return (
+    <>
+      <NavigationMenu>
+        <NavigationMenuItem>
+
+        </NavigationMenuItem>
+      </NavigationMenu>
+    </>
+  )
+}
