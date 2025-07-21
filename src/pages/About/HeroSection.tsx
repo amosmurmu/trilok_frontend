eba73271958e8859b17862d29c76488b828636eb
amosmurmu <amosmurmu002@gmail.com>
2025-07-21 22:39:28 +0530

    fix: about us heading

--- src/pages/About/HeroSection.tsx
+++ src/pages/About/HeroSection.tsx
@@ -11,8 +11,8 @@ export const HeroSection = () => {
         <div className="absolute inset-0 bg-black/50"></div>
       </div>
 
-      <div className="absolute left-40 bottom-[80px] sm:bottom-[90px] md:bottom-[95px] lg:bottom-[100px] px-4 sm:px-6 md:px-8 lg:px-10">
-        <h1 className="text-white text-[32px] sm:text-[40px] md:text-[50px] text-center lg:text-[60px] font-semibold leading-tight">
+      <div className="absolute inset-0 flex items-end justify-center lg:justify-start px-4 sm:px-6 md:px-8 lg:px-10 pb-[80px] sm:pb-[90px] md:pb-[95px] lg:pb-[100px]">
+        <h1 className="text-white text-[32px] sm:text-[40px] md:text-[50px] lg:text-[60px] font-semibold leading-tight text-center lg:text-left max-w-screen-xl w-full">
           About Us
         </h1>
       </div>
